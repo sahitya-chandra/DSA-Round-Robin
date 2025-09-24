b9c17d176cf4e149d0aad21878dee8a1321746b8
Sahitya Chandra <sahityajb@gmail.com>
2025-09-24 22:41:48 +0530

    optimized docker cmd

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -8,36 +8,24 @@ dotenv.config();
 createCodeWorker(async (job) => {
   const { code } = job.data;
 
-	const tempDir = path.join(process.env.HOME || "", "docker_temp");
+	const tempDir = path.join(String(process.env.HOME), "docker_temp");
 	if (!fs.existsSync(tempDir)) fs.mkdirSync(tempDir, { recursive: true });
 
 	const filePath = path.join(tempDir, `job-${job.id}.cpp`);
 	fs.writeFileSync(filePath, code as string);
 
+  console.log("Mount directory:", tempDir);
   console.log("C++ file created at:", filePath);
 
-  const dockerCmd = `
-    docker run --rm \
-    -v ${filePath}:/code/main.cpp \
-    --memory=128m --cpus=0.5 \
-    gcc:latest bash -c '
-      echo "=== Compiling ===";
-      g++ /code/main.cpp -o /code/main 2>&1;
-      COMPILATION_STATUS=$?;
-      if [ $COMPILATION_STATUS -ne 0 ]; then
-      echo "=== Compilation Failed ===";
-      exit $COMPILATION_STATUS;
-      fi
-      echo "=== Running Program ===";
-      /code/main
-  '
-  `;
+  const dockerCmd = `docker run --rm -v ${tempDir}:/code --memory=128m --cpus=0.5 gcc:latest bash -c "g++ /code/job-${job.id}.cpp -o /code/main && /code/main"`;
 
  try {
     const result = await new Promise<string>((resolve, reject) => {
       exec(dockerCmd, 
-        { timeout: 5000 }, 
+        { timeout: 10000 }, 
         (err, stdout, stderr) => {
+        console.log('STDOUT:', stdout);
+        console.log('STDERR:', stderr);
         if (err) return reject(stderr || err.message);
         resolve(stdout || stderr);
       });
