35f0135c26942fc2fea070bf6ee279232a34ec8b
DiyanshuVortex <divyanshuchandra9027@gmail.com>
2025-10-17 10:48:22 +0530

    add social router and implement friend-related API endpoints

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,7 +4,7 @@ import { PORT } from "./config/config";
 import { codeQueue, queueEvents } from "@repo/queue";
 import prisma from "@repo/db";
 import { Testcase } from "@repo/types";
-
+import { socialRouter } from "./routes/socialRouter";
 
 const app = express();
 app.use(
@@ -14,6 +14,8 @@ app.use(
   })
 );
 app.use(express.json());
+
+app.use("/social" , socialRouter )
 
 app.post("/api/submit", async (req, res) => {
   const {id, code, language } = req.body;
