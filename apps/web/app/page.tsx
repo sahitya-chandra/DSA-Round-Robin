bc39e51c8f066559a67493857bcaaec13a955883
Sahitya Chandra <sahityajb@gmail.com>
2025-11-19 23:08:41 +0530

    Revert "minor-chores"

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,7 +4,6 @@ import { headers } from "next/headers";
 import { Hero } from "@/components/Landing/Hero";
 import { Features } from "@/components/Landing/Features";
 import { Footer } from "@/components/Landing/Footer";
-import { Navbar } from "@/components/Navbar";
 
 const Page = async () => {
   const session = await auth.api.getSession({
@@ -12,8 +11,7 @@ const Page = async () => {
   });
 
   return (
-     <main className="min-h-screen bg-slate-950 text-white selection:bg-cyan-500/30">
-      <Navbar />
+    <main className="min-h-screen bg-slate-950 text-white selection:bg-cyan-500/30">
       <Hero userId={session?.user.id} />
       <Features />
       <Footer />
