86efcb2dda6ada23d2578cf5db188382922aec5d
Sahitya Chandra <sahityajb@gmail.com>
2025-12-27 20:15:19 +0530

    chore

--- apps/server/src/config/config.ts
+++ apps/server/src/config/config.ts
@@ -3,6 +3,9 @@ dotenv.config({ path: "../../.env" });
 
 export const PORT = process.env.SERVER_PORT || 5000;
 
+export const BETTER_AUTH_SECRET = process.env.BETTER_AUTH_SECRET || "xyz"
+export const BETTER_AUTH_URL = process.env.BETTER_AUTH_URL || "http://localhost:3000"
+
 // Support multiple origins for CORS (comma-separated)
 const clientUrlEnv = process.env.CLIENT_URL || "http://localhost:3000";
 export const CLIENT_URL = clientUrlEnv.includes(',') 
