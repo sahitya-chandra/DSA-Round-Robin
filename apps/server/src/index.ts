9a352a9a236f04b64ba53a78dfcded56fa313ee0
Sahitya Chandra <sahityajb@gmail.com>
2025-12-30 12:22:52 +0530

    judge0 integration logic

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -5,12 +5,14 @@ import { PORT } from "./config/config";
 import { matchSweeper } from "./sockets/matchSweeper";
 import { setupChatSocket } from "./sockets/chatsocket";
 import { app } from "./app";
+import { initJudge0Worker } from "./workers/judge0Worker";
 
 const server = http.createServer(app);
 export const io = initIo(server);
 
 setupSockets(io);
 setupChatSocket(io);
+initJudge0Worker();
 
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
